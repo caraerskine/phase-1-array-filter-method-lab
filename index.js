7bfd703f868eeeb514dcab2307b9c858b022ddde
CaraErskine <caraerskine@gmail.com>
2022-06-04 18:48:49 -0400

    commit tree 58f43b96840f488eda596b7b88b934a527e23dc1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+
+function findMatching(names, name){
+
+const result = names.filter(driver => driver.toUpperCase() === name.toUpperCase())
+return result 
+console.log(result);
+}
+
+
+function fuzzyMatch(nameNames, letters){
+ 
+const result = nameNames.filter(driver => driver.slice(0, letters.length) === letters)
+ return result
+ console.log(result);
+}
+
+function matchName(nameNames, letters){
+    const result = nameNames.filter(driver => driver.name === letters);
+  return result
+}
